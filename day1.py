b4571e5bbf25d9938ca1afa1f84bb09e93066263
shiyuzhen1020 <616803804@qq.com>
2021-07-04 16:18:38 +0800

    第一次向github提交代码

---
+++ day1.py
@@ -0,0 +1,40 @@
+"""
+print ("hello,world") #字符串
+print(12346)          #整数
+print(1.11)           #小数
+print(True,False)     #布尔值
+print(())             #元祖
+print([])             #数组
+print({})             #字典
+print("字符串"+"拼接") #字符串拼接
+print("字符串"*10)
+print(((1+2)*99-100)*2)
+print(2>3)
+print(1+1==2)
+"""
+#a=input ("请输入内容：")
+#print("input获取的内容：",a)
+# a=input("请输入a：")
+# b=input("请输入b：")
+# print("input获取的内容：",a+b)
+#数据类型#
+# print(type("aaa"))
+# print(type(111))
+# print(type(1.11))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+#字符串转换成其它类型数据，必须满足‘长得像’
+# a=float(input("请输入a："))
+# b=float(input("请输入b："))
+# print("input获取的内容：",a+b)
+#len()方法的使用
+# a='hghhjhj  jhf'
+# print(len(a))
+
+a="学习python的第一天"
+b="要继续加油!"
+print(a)
+print(b)
+print("a+b的长度：",len(a)+len(b))
